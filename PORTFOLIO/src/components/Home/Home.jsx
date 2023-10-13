2247c27da44edcce0e35a790e1c5c64feb790008
Akadus23 <davilgo23@gmail.com>
2023-10-13 15:31:07 -0500

    home avanzado

--- PORTFOLIO/src/components/Home/Home.jsx
+++ PORTFOLIO/src/components/Home/Home.jsx
@@ -2,16 +2,17 @@ import React from "react";
 import Column from "../Column/Column";
 
 
+
 export default function Home () {
 
     let central =
         <div class="grid grid-cols-[repeat(2,1fr)] grid-rows-[1fr] w-[100%] h-[100%] ">
             <div class=" flex flex-col items-center justify-center ">
-                <div class=" w-[100%] h-2/5 grid grid-cols-[1fr] grid-rows-[repeat(4,1fr)]]  text-left">
+                <div class=" w-[100%] h-2/5 grid grid-cols-[1fr] grid-rows-[repeat(4,1fr)]] text-left">
                     <div class="  text-white text-2xl flex  content-end items-end justify-start translate-y-5" >Hi! I'm Davinson</div>
-                    <div class=" -ml-1 text-[56px] h-20  bg-clip-text text-transparent bg-gradient-to-r from-[#22007C] to-[#531fff] " >  Full Stack Developer</div>
+                    <div class=" -ml-1 text-[56px] h-20  bg-clip-text text-transparent bg-gradient-to-r from-[#ffb703] to-[#4502ff] " >  Full Stack Developer</div>
                     <div class=" text-white text-2xl -translate-y-6 " >Isn't it fun to create?</div>
-                    <div class="  flex flex-row items-center content-start justify-start" >
+                    <div class="  flex flex-row items-center content-start justify-start -translate-y-5" >
                         <button class="w-28 h-12 border-2 rounded-md border-[#ffb703] text-[#ffb703] "> Resume </button>
                         
                         <button class="w-28 h-12 border-2 rounded-md border-[#ffb703] text-[#ffb703] ml-5"> {"Projects ->"} </button>
@@ -19,6 +20,16 @@ export default function Home () {
                 </div>
             
                 
+            </div>
+            <div class=" flex flex-col items-center justify-center " >
+                <div class="w-96 h-96 relative">
+                <div class="w-96 h-96 outline-dashed outline-offset-2 animate-spin outline-[#4400ff] rounded-full bg-cover absolute  bg-right-top  " >
+                </div>
+                    <div class="w-96 h-96 rounded-full bg-cover bg-right-top object-cover" style={{ backgroundImage: 'url("/foto2.jpg")' }}>
+
+                    </div>
+
+                </div>
             </div>
             
         </div>
