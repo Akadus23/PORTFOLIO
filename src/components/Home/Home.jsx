6c12126a1eaca197329e993f33bde89ce4dd4e67
Akadus23 <davilgo23@gmail.com>
2023-11-08 20:43:23 -0500

    title fixed

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -11,7 +11,7 @@ export default function Home() {
             <div class=" flex flex-col items-center justify-center ">
                 <div class=" w-[100%] h-2/5 grid grid-cols-[1fr] grid-rows-[repeat(4,1fr)]] text-left ">
                     <div class="  text-slate-300 text-2xl flex content-end items-end justify-start translate-y-5 " ><span class="text-white" >Hi</span><span class="text-[#ffb703] mr-2 " >!</span>I'm Davinson <img class="w-6 ml-2 mb-2" src="/static/img/hand.svg" alt=""></img> </div>
-                    <div class=" -ml-1 text-[56px] h-20  bg-clip-text text-transparent bg-gradient-to-r from-[#ffb703] to-[#4502ff] " >  Full Stack Developer</div>
+                    <div class=" -ml-1 w-full text-[56px] text-clip h-20 bg-clip-text text-transparent bg-gradient-to-r from-[#ffb703] to-[#4502ff] " >Full Stack Dev<span class="text-[#ffb703]">.</span></div>
                     <div class=" text-slate-300 text-2xl -translate-y-5 " >Isn't it fun to create?</div>
                     <div class="  flex flex-row items-center content-start justify-start -translate-y-5" >
                     <a href="/src/archivos/Resume_DavinsonVillamizar_DevFullStack.pdf" download="Resume_DavinsonVillamizar_DevFullStack" ><button class="w-28 h-12 border-2 rounded-md border-[#ffb703] text-[#ffb703] flex flex-row items-center justify-center gap-1 ">Resume<img class="w-5" src="/static/img/pdf.png" ></img></button></a>
